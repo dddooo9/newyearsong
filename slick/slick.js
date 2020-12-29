0e1cd42855034122f0b3ed1acc18185012507b7b
dddooo9 <dlaehdus99@naver.com>
2020-12-29 15:07:15 +0900

    type randomize

--- slick/slick.js
+++ slick/slick.js
@@ -3009,3 +3009,21 @@
     };
 
 }));
+
+$.fn.randomize = function (selector) {
+    var $elems = selector ? $(this).find(selector) : $(this).children(),
+        $parents = $elems.parent();
+
+    $parents.each(function () {
+        $(this).children(selector).sort(function (childA, childB) {
+            // * Prevent last slide from being reordered
+            if($(childB).index() !== $(this).children(selector).length - 1) {
+                return Math.round(Math.random()) - 0.5;
+            }
+        }.bind(this)).detach().appendTo(this);
+    });
+
+    return this;
+};
+
+$(".slider").randomize().slick();
